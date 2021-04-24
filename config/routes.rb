430df9d0ff5e24e814630d16e74607dc6deba5f1
zhutongtong <zhutongtong@360.cn>
2021-04-24 17:55:10 +0800

    update gemfile for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # get 'users/new'
-  get '/signup', to: 'users#new'
-  get '/help', to: 'static_pages#help'
+  get "/signup", to: "users#new"
+  get "/help", to: "static_pages#help"
   # 这个规则会定义两个具名路由
   # help_path -> '/help'
   # help_url -> 'http://www.example.com/help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  root 'static_pages#home'
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  root "static_pages#home"
   # users是一个资源 满足restful
   resources :users
 end
